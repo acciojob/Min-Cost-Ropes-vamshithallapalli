5ab362f6a7375041fc0b4b3c8b4cbaf1c755ca61
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-06-10 09:31:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+	if(arr.length === 1) return 0;
+
+	let heap = [...arr];
+	heap.sort((a,b) => a-b);
+
+	let cost = 0;
+
+	while(heap.size > 1){
+		let first = heap.shift();
+		let second = heap.shift();
+
+		let newRope = first + second;
+        cost += newRope;
+
+		heap.push(newRope);
+
+		heap.sort((a,b) => a-b);
+		
+	}
+	return cost;
 }
 
 module.exports=mincost;
